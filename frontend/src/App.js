01e58d528401236356cc76391bf516a1ad002d83
mdhm98 <muhd.haameed@gmail.com>
2022-03-26 20:30:00 +0800

    Added Strava Integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,58 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React, { Component, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import getWeb3 from "./getWeb3";
+import Home from "./components/Home";
+import UploadActivity from "./components/UploadActivity";
+import ConnectStrava from "./components/ConnectStrava";
+import { AppBar, CircularProgress, Toolbar, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import Token from "./components/Token";
 
-import "./App.css";
-
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
-
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
+const useStyles = makeStyles(() => {
+  return {
+    home: {
+      justifyContent: "center",
+      display: "flex",
+      padding: "300px 300px 300px 300px",
+    },
   };
+});
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+function App() {
+  const classes = useStyles();
+  // state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  const [storageValue, setStorageValue] = useState(0);
+  const [web3, setWeb3] = useState(null);
+  const [accounts, setAccounts] = useState(null);
+  const [contract, setContract] = useState(null);
+
+  // componentDidMount = async () => {
+  //   try {
+  //     // Get network provider and web3 instance.
+  //     const web3 = await getWeb3();
+  //     // Use web3 to get the user's accounts.
+  //     const accounts = await web3.eth.getAccounts();
+  //     // Get the contract instance.
+  //     const networkId = await web3.eth.net.getId();
+  //     const deployedNetwork = SimpleStorageContract.networks[networkId];
+  //     const instance = new web3.eth.Contract(
+  //       SimpleStorageContract.abi,
+  //       deployedNetwork && deployedNetwork.address
+  //     );
+  //     // Set web3, accounts, and contract to the state, and then proceed with an
+  //     // example of interacting with the contract's methods.
+  //     this.setState({ web3, accounts, contract: instance }, this.runExample);
+  //   } catch (error) {
+  //     // Catch any errors for any of the above operations.
+  //     alert(
+  //       `Failed to load web3, accounts, or contract. Check console for details.`
+  //     );
+  //     console.error(error);
+  //   }
+  // };
+
+  const runExample = async () => {
+    // const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
     await contract.methods.set(5).send({ from: accounts[0] });
@@ -45,29 +61,42 @@ class App extends Component {
     const response = await contract.methods.get().call();
 
     // Update state with the result.
-    this.setState({ storageValue: response });
+    // this.setState({ storageValue: response });
+    setStorageValue(response);
   };
 
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
-  }
+  // if (!web3) {
+  //   return (
+  //     <div>
+  //       <AppBar position="static">
+  //         <Toolbar>
+  //           <Typography variant="h1">HabitChain</Typography>
+  //         </Toolbar>
+  //       </AppBar>
+  //       <div className={classes.home}>
+  //         <CircularProgress />
+  //       </div>
+  //     </div>
+  //   );
+  // } else {
+  return (
+    <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h1">HabitChain</Typography>
+        </Toolbar>
+      </AppBar>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/fitnessTracker" exact element={<UploadActivity />} />
+          <Route path="/connectTracker" exact element={<ConnectStrava />} />
+          <Route path="/token" exact element={<Token />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+  //}
 }
 
 export default App;
